0e701bef78a7e329aff90f268d9eb58db3e94f72
alghrbe155-prog <alghrbe155@gmail.com>
2026-06-16 07:40:10 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,7 @@
 const CACHE_NAME = 'accountant-v1';
-const urlsToCache = [
-  '.',
-  'index.html',
-  'manifest.json'
-];
-
-// تثبيت الـ Service Worker
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['.', 'index.html', 'manifest.json'])));
 });
-
-// استرجاع الملفات من الكاش عند عدم الاتصال
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-// تحديث الكاش (حذف القديم)
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
